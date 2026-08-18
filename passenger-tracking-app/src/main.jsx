09b8dfb17a6aac6a6c85140a23cc09c7fec837de
erayovunc55 <eray.ovunc@gmail.com>
2026-08-18 18:55:41 +0300

    Wire passenger tracking language provider

--- passenger-tracking-app/src/main.jsx
+++ passenger-tracking-app/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "leaflet/dist/leaflet.css";
 import "./styles.css";
 import App from "./App";
+import { LanguageProvider } from "./i18n";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
 );
